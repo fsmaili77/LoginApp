ca727eeb6e1be0cc581e6c769946ddb4ae50abc4
Fehmir Smaili <fehmir.smaili@uha.fr>
2021-07-23 23:39:54 +0200

    Preparing user interface

---
+++ frontend/src/LoginForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class LoginForm extends React.Component {
+  render(){
+
+  
+  return (
+    <div className="loginForm">
+     Yupi
+    </div>
+  );
+}
+}
+
+export default LoginForm;
---
+++ frontend/src/SubmitButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class SubmitButton extends React.Component {
+  render(){
+
+  
+  return (
+    <div className="submitButton">
+     Yupi
+    </div>
+  );
+}
+}
+
+export default SubmitButton;
