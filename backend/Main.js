a3fe38a0fa2a3e9705089c8ac36f5e57b5ee0c10
Fehmir Smaili <fehmir.smaili@uha.fr>
2021-07-25 16:10:40 +0200

    Modification of Main.js

--- backend/Main.js
+++ backend/Main.js
@@ -6,7 +6,7 @@ const session    = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
 const Router     = require('./Router');
 
-app.use(express.static(path.join(__dirname, 'buld')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json());
 
 //Databse
